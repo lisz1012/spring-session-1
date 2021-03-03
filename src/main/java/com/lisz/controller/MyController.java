01ce5819f79922945a5462151302f422a358f42e
lisz1012 <lisz1012@163.com>
2021-03-02 23:17:15 -0800

    服务端向Session里面写东西，通过Redis也能共享给其他的服务。添加笔记

--- src/main/java/com/lisz/controller/MyController.java
+++ src/main/java/com/lisz/controller/MyController.java
@@ -3,10 +3,17 @@ package com.lisz.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class MyController {
-	@GetMapping("/list")
-	public String list(){
-		return "list";
+	@GetMapping("/setSessionAttribute")
+	public String setSessionAttribute(HttpServletRequest request){
+		request.getSession().setAttribute("a", 1);
+		return "Session attribute has been set";
+	}
+	@GetMapping("/getSessionAttribute")
+	public String getSessionAttribute(HttpServletRequest request){
+		return "Session attribute: " + request.getSession().getAttribute("a");
 	}
 }
